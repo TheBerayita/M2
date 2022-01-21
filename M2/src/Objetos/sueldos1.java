2bbac4bdfcebd9119fd3168845e80ab3129fd5cb
TheBerayita <97508463+TheBerayita@users.noreply.github.com>
2022-01-21 13:35:18 +0100

    Milestone2

--- M2/src/Objetos/sueldos1.java
+++ M2/src/Objetos/sueldos1.java
@@ -4,28 +4,25 @@ public class sueldos1 {
 	public static void calcularsueldo(trabajador t1) {
 		switch(t1.rol) {
 		case boss:
-			t1.sueldobruto*=1.50;
-			t1.sueldoneto=t1.sueldobruto*0.68;
+			t1.sueldo*=1.50;
 			break;
 		case manager:
-			t1.sueldobruto*=1.10;
-			t1.sueldoneto=t1.sueldobruto*0.74;
+			t1.sueldo*=1.10;
+			break;
+		case employee:
+			t1.sueldo*=0.85;
 			break;
 		case senior:
-			t1.sueldobruto*=0.95;
-			t1.sueldoneto=t1.sueldobruto*0.76;
+			t1.sueldo*=0.95;
 			break;
 		case mid:
-			t1.sueldobruto*=0.90;
-			t1.sueldoneto=t1.sueldobruto*0.85;
+			t1.sueldo*=0.90;
 			break;
 		case junior:
-			t1.sueldobruto*=0.85;
-			t1.sueldoneto=t1.sueldobruto*0.98;
+			t1.sueldo*=0.85;
 			break;
 		case volunteer:
-			t1.sueldobruto*=0;
-			t1.sueldoneto=t1.sueldobruto+300;
+			t1.sueldo*=0;
 			break;
 	}
 	}
--- M2/src/Objetos/trabajador.java
+++ M2/src/Objetos/trabajador.java
@@ -3,23 +3,17 @@ package Objetos;
 public class trabajador {
 	
 	protected Rol rol;
-	protected double sueldobruto;
-	protected double sueldoneto;
+	protected double sueldo;
 	
 	public trabajador(Rol rol, double sueldo) {
 		this.rol = rol;
-		this.sueldobruto = sueldo;
-		this.sueldoneto=0;
+		this.sueldo = sueldo;
 	}
 
 	@Override
 	public String toString() {
-		if(this.rol==rol.volunteer) {
-			return "trabajador [rol=" + rol + ", sueldobruto=" + sueldobruto + ", sueldoneto con ayudas=" + sueldoneto + "]";
-		}
-		return "trabajador [rol=" + rol + ", sueldobruto=" + sueldobruto + ", sueldoneto=" + sueldoneto + "]";
+		return "trabajador [rol=" + rol + ", sueldo=" + sueldo + "]";
 	}
-
-
+	
 	
 }
