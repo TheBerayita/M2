f3fc76b2c3a0548c335600aab7ba101a0a068e95
TheBerayita <97508463+TheBerayita@users.noreply.github.com>
2022-01-24 09:28:16 +0100

    Commit

--- M2/src/Objetos/sueldos1.java
+++ M2/src/Objetos/sueldos1.java
@@ -2,31 +2,44 @@ package Objetos;
 
 public class sueldos1 {
 	public static void calcularsueldo(trabajador t1) {
-		switch(t1.rol) {
+		switch (t1.rol) {
 		case boss:
-			t1.sueldobruto*=1.50;
-			t1.sueldoneto=t1.sueldobruto*0.68;
+			t1.sueldobruto *= 1.50;
+			t1.sueldoneto = t1.sueldobruto * 0.68;
+			t1.sueldobrutoanual = t1.sueldobruto * 12;
+			t1.sueldonetoanual = t1.sueldoneto * 12;
 			break;
 		case manager:
-			t1.sueldobruto*=1.10;
-			t1.sueldoneto=t1.sueldobruto*0.74;
+			t1.sueldobruto *= 1.10;
+			t1.sueldoneto = t1.sueldobruto * 0.74;
+			t1.sueldobrutoanual = t1.sueldobruto * 12;
+			t1.sueldonetoanual = t1.sueldoneto * 12;
 			break;
 		case senior:
-			t1.sueldobruto*=0.95;
-			t1.sueldoneto=t1.sueldobruto*0.76;
+			t1.sueldobruto *= 0.95;
+			t1.sueldoneto = t1.sueldobruto * 0.76;
+			t1.sueldobrutoanual = t1.sueldobruto * 12;
+			t1.sueldonetoanual = t1.sueldoneto * 12;
 			break;
 		case mid:
-			t1.sueldobruto*=0.90;
-			t1.sueldoneto=t1.sueldobruto*0.85;
+			t1.sueldobruto *= 0.90;
+			t1.sueldoneto = t1.sueldobruto * 0.85;
+			t1.sueldobrutoanual = t1.sueldobruto * 12;
+			t1.sueldonetoanual = t1.sueldoneto * 12;
 			break;
 		case junior:
-			t1.sueldobruto*=0.85;
-			t1.sueldoneto=t1.sueldobruto*0.98;
+			t1.sueldobruto *= 0.85;
+			t1.sueldoneto = t1.sueldobruto * 0.98;
+			t1.sueldobrutoanual = t1.sueldobruto * 12;
+			t1.sueldonetoanual = t1.sueldoneto * 12;
 			break;
 		case volunteer:
-			t1.sueldobruto*=0;
-			t1.sueldoneto=t1.sueldobruto+300;
+			t1.sueldobruto *= 0;
+			t1.sueldoneto = t1.sueldobruto + 300;
+			t1.sueldobrutoanual = t1.sueldobruto * 12;
+			t1.sueldonetoanual = t1.sueldoneto * 12;
+
 			break;
-	}
+		}
 	}
 }
--- M2/src/Objetos/trabajador.java
+++ M2/src/Objetos/trabajador.java
@@ -5,19 +5,23 @@ public class trabajador {
 	protected Rol rol;
 	protected double sueldobruto;
 	protected double sueldoneto;
+	protected double sueldonetoanual;
+	protected double sueldobrutoanual;
 	
 	public trabajador(Rol rol, double sueldo) {
 		this.rol = rol;
 		this.sueldobruto = sueldo;
 		this.sueldoneto=0;
+		this.sueldobrutoanual=0;
+		this.sueldonetoanual=0;
 	}
 
 	@Override
 	public String toString() {
 		if(this.rol==rol.volunteer) {
-			return "trabajador [rol=" + rol + ", sueldobruto=" + sueldobruto + ", sueldoneto con ayudas=" + sueldoneto + "]";
+			return "trabajador [rol= " + rol + ", sueldobruto= " + sueldobruto + ", sueldoneto con ayudas= " + sueldoneto + " , sueldo neto anual es = "+ sueldonetoanual+", el sueldo bruto anual es = "+sueldobrutoanual+"]";
 		}
-		return "trabajador [rol=" + rol + ", sueldobruto=" + sueldobruto + ", sueldoneto=" + sueldoneto + "]";
+		return "trabajador [rol= " + rol + ", sueldobruto= " + sueldobruto + ", sueldoneto= " + sueldoneto + " , sueldo neto anual es = "+ sueldonetoanual+", el sueldo bruto anual es = "+sueldobrutoanual+"]";
 	}
 
 
